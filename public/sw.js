c6422351cf9fb745d014345e50504b5b175a54ad
Martin <tinchoccr@gmail.com>
2026-09-15 12:09:23 -0300

    Bump service worker cache to v4 for instant auto-update

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Service Worker for RielAR Web App (iOS & Android Zero-Install)
-const CACHE_NAME = 'rielar-v3';
+const CACHE_NAME = 'rielar-v4';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
